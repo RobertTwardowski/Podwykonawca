1cbeda9024db0b76c8100e01b22370d2859b8d45
Robert <twardowski.robert1908@gmail.com>
2023-03-09 16:13:35 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/organisms/Navigation/Navigation'
 import { GlobalStyle } from './assets/styles/GlobalStyles'
 import SearchForm from './components/organisms/SearchForm/SearchForm.js'
 import Companies from './components/organisms/Companies.js/Companies.js'
+import Footer from './components/organisms/Footer/Footer.js'
 
 function App () {
   return (
@@ -12,6 +13,7 @@ function App () {
       <Navigation />
       <SearchForm />
       <Companies />
+      <Footer/>
     </>
   )
 }
