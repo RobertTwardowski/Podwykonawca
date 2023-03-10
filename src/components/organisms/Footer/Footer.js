48b582a2e2b23304febcfb7e7b4b6c344604468c
Robert <twardowski.robert1908@gmail.com>
2023-03-10 09:29:04 +0100

    icons

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FooterStyle } from './Footer.style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
  const Footer = () => {
   return (
-    <div>Footer</div>
+    <FooterStyle><div>Footer <FontAwesomeIcon icon="fa-solid fa-check-square" /></div>
+ </FooterStyle>
   )
 }
 export default Footer
---
+++ src/components/organisms/Footer/Footer.style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FooterStyle = styled.div`
+width: 100%;
+height: 300px;
+background-color: #5983FC;
+justify-content: center;
+align-items: center;
+text-align: center;
+`
