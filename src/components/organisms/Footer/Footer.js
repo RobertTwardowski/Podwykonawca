037acd6d7a8f6c5320ed65d665065f5b2f80b32a
Robert <twardowski.robert1908@gmail.com>
2023-03-16 14:36:06 +0100

    footer

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { FooterStyle } from './Footer.style'
+import { FooterStyle, IconStyle } from './Footer.style'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import {
+  faFacebook,
+  faGoogle,
+  faInstagram,
+  faTwitter
+} from '@fortawesome/free-brands-svg-icons'
 
-
- const Footer = () => {
+const Footer = () => {
   return (
-    <FooterStyle><div>Footer <FontAwesomeIcon icon={faFacebook} /> <FontAwesomeIcon icon={faTwitter} /></div>
- </FooterStyle>
+    <FooterStyle>
+      <IconStyle>
+       <a href='http://www.facebook.com' target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faFacebook} size="4x"/></a>
+       <a href="http://www.twitter.com" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faTwitter} size="4x"/></a>
+       <a href ="http://www.instagram.com" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faInstagram} size="4x"/></a>
+       <a href='http://www.google.com/' target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faGoogle} size="4x"/></a>
+      </IconStyle>
+    </FooterStyle>
   )
 }
 export default Footer
--- src/components/organisms/Footer/Footer.style.js
+++ src/components/organisms/Footer/Footer.style.js
@@ -8,3 +8,19 @@ justify-content: center;
 align-items: center;
 text-align: center;
 `
+export const IconStyle = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 100%;
+height: 100%;
+
+a{
+    padding: 1rem;
+    text-decoration: none;
+    color: #444;
+}
+a:hover{
+    color:#999;
+}
+`
