c91586efff4f60debd1e9dbf352499e0141e54e3
Robert Twardowski <twardowski.robert1908@gmail.com>
2023-05-21 09:22:43 +0200

    profile

--- src/components/molecules/Company/CompanyAnnouncement.js
+++ src/components/molecules/Company/CompanyAnnouncement.js
@@ -1,49 +1,58 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { companyData, comments } from '../../../Data/Data';
-import { Wrapper, Title, About, SectionComment, Section } from './CompanyAnnouncement.styles';
-import { Button } from '../../atom/ButtonMoreInfo.styles';
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { companyData, comments } from '../../../Data/Data'
+import {
+  Wrapper,
+  CommentAdd,
+  Title,
+  About,
+  SectionComment,
+  Section,
+  CommentInput,
+  Comments
+} from './CompanyAnnouncement.styles'
+import { Button } from '../../atom/ButtonMoreInfo.styles'
 
-function CompanyAnnouncement() {
-  const { id } = useParams();
-  const Company = companyData.find((Company) => Company.id == id);
-  const [name, setName] = useState('');
-  const [comment, setComment] = useState('');
-  const [rating, setRating] = useState(''); // Updated to use a select option
-  const [commentIndex, setCommentIndex] = useState(0);
+function CompanyAnnouncement () {
+  const { id } = useParams()
+  const Company = companyData.find(Company => Company.id == id)
+  const [name, setName] = useState('')
+  const [comment, setComment] = useState('')
+  const [rating, setRating] = useState('') // Updated to use a select option
+  const [commentIndex, setCommentIndex] = useState(0)
 
-  const visibleComments = comments.slice(commentIndex, commentIndex + 5);
+  const visibleComments = comments.slice(commentIndex, commentIndex + 5)
 
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
+  const handleNameChange = e => {
+    setName(e.target.value)
+  }
 
-  const handleCommentChange = (e) => {
-    setComment(e.target.value);
-  };
+  const handleCommentChange = e => {
+    setComment(e.target.value)
+  }
 
-  const handleRatingChange = (e) => {
-    setRating(e.target.value);
-  };
+  const handleRatingChange = e => {
+    setRating(e.target.value)
+  }
 
   const handleCommentSubmit = () => {
     if (name.trim() !== '' && comment.trim() !== '' && rating !== '') {
       const newComment = {
         commentName: name,
         commnet: comment,
-        rating: rating,
-      };
+        rating: rating
+      }
 
-      comments.unshift(newComment);
-      setName('');
-      setComment('');
-      setRating('');
+      comments.unshift(newComment)
+      setName('')
+      setComment('')
+      setRating('')
     }
-  };
+  }
 
   const loadMoreComments = () => {
-    setCommentIndex((prevIndex) => prevIndex + 5);
-  };
+    setCommentIndex(prevIndex => prevIndex + 5)
+  }
 
   return (
     <Section>
@@ -60,36 +69,46 @@ function CompanyAnnouncement() {
         </About>
       </Wrapper>
       <SectionComment>
-        <div>
-          {visibleComments.map((comment, index) => (
-            <div key={index}>
-              <p>{comment.commentName}</p>
-              <p>{comment.commnet}</p>
-              <p>{comment.rating}</p>
-            </div>
-          ))}
+        {visibleComments.map((comment, index) => (
+          <Comments key={index}>
+            <p>{comment.commentName}</p>
+            <p>{comment.commnet}</p>
+            <p>{comment.rating}</p>
+          </Comments>
+        ))}
 
-          {comments.length > commentIndex + 5 && (
-            <Button onClick={loadMoreComments}>Pokaż kolejne komentarze</Button>
-          )}
+        {comments.length > commentIndex + 5 && (
+          <Button onClick={loadMoreComments}>Pokaż kolejne komentarze</Button>
+        )}
 
-          <div>
-            <input type="text" placeholder="Imię" value={name} onChange={handleNameChange} />
-            <input type="text" placeholder="Komentarz" value={comment} onChange={handleCommentChange} />
-            <select value={rating} onChange={handleRatingChange}>
-              <option value="">Wybierz ocenę</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-            <Button onClick={handleCommentSubmit}>Dodaj komentarz</Button>
-          </div>
-        </div>
+        <CommentAdd>
+          <input
+            type='text'
+            placeholder='Imię'
+            value={name}
+            onChange={handleNameChange}
+          />
+          <select value={rating} onChange={handleRatingChange}>
+            <option value=''>Wybierz ocenę</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
+          </select>
+          <CommentInput
+            type='text'
+            placeholder='Komentarz'
+            value={comment}
+            onChange={handleCommentChange}
+            maxLength={400}
+          />
+
+          <Button onClick={handleCommentSubmit}>Dodaj komentarz</Button>
+        </CommentAdd>
       </SectionComment>
     </Section>
-  );
+  )
 }
 
-export default CompanyAnnouncement;
+export default CompanyAnnouncement
--- src/components/molecules/Company/CompanyAnnouncement.styles.js
+++ src/components/molecules/Company/CompanyAnnouncement.styles.js
@@ -12,8 +12,8 @@ export const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   text-align: center;
+  height: 600px;
   width: 70%;
-  height: 700px;
 `
 
 export const Title = styled.div`
@@ -33,4 +33,48 @@ p{
 export const SectionComment=styled.div`
 width: 20%;
 margin: 20px;
+display: flex;
+justify-content: center;
+flex-direction: column;
+text-align: center;
+align-items: center;
+background-color: #d4ebf2;
+`
+export const CommentAdd=styled.div`
+display: flex;
+justify-content: center;
+flex-direction: column;
+background-color: #f2f4f5;
+margin: 20px;
+width: 90%;
+
+
+input,select{
+  height: 30px;
+  
+  margin: 10px;
+  border-radius: 6px;
+  font-size: 16px;
+}
+`
+
+export const CommentInput=styled.textarea`
+height: 200px;
+background-color: #f2f4f5;
+margin: 10px;
+font-size: 16px;
+border-radius: 6px;
+`
+export const Comments=styled.div`
+width: 90%;
+display: flex;
+margin:20px;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color: #f2f4f5;
+border-radius: 6px;
+p{
+  padding: 10px;
+}
 `
